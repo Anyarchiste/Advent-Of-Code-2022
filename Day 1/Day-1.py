ada2863e7f3993357f2c829a9056427880f52fce
Anyarchiste <98529860+Anyarchiste@users.noreply.github.com>
2022-12-01 11:17:10 +0100

    Day 1
    my code for day 1

---
+++ Day 1/Day-1.py
@@ -0,0 +1,52 @@
+
+# Challenge 1
+def get_data():
+    liste_line_b = []
+    with open("Calories_per_item.txt", "r") as fp:
+        for line in fp:
+            liste_line_b.append(line)
+
+    print(liste_line_b)
+    return liste_line_b
+
+
+def treat_data(liste_line_b):
+    liste_line = []
+    for e in liste_line_b:
+        liste_line.append(e.replace("\n", ""))
+
+    print(liste_line)
+
+    list_per_elf_def = []
+    list_per_x_elf = []
+    for n in liste_line:
+        if n == "":
+            list_per_elf_def.append(list_per_x_elf)
+            list_per_x_elf = []
+        else:
+            list_per_x_elf.append(n)
+
+    list_per_elf_def.append(list_per_x_elf)
+
+    print(list_per_elf_def)
+    return list_per_elf_def
+
+
+raw_data = get_data()
+list_per_elf = treat_data(raw_data)
+
+calories_per_elf = []
+for s in list_per_elf:
+    total_per_elf = 0
+    for r in s:
+        total_per_elf += int(r)
+        calories_per_elf.append(total_per_elf)
+
+print(calories_per_elf)
+
+print(sorted(calories_per_elf, reverse=True))
+
+# Challenge 2
+top_3 = sorted(calories_per_elf, reverse=True)[0] + sorted(calories_per_elf, reverse=True)[1] + sorted(calories_per_elf, reverse=True)[2]
+
+print(top_3)
